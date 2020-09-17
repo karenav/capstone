aa9c771c6578a17905010cc5f4d9aea329e02c8e
Karen Avner <karenav@google.com>
2020-09-17 13:50:13 +0000

    add Place class

---
+++ src/main/webapp/Place.java
@@ -0,0 +1,17 @@
+import com.google.auto.value.AutoValue;
+
+@AutoValue 
+public abstract class Place {
+  public abstract String name();
+  public abstract String website();
+  public abstract String description();
+  public abstract int phone();
+  public abstract Long longitude();
+  public abstract Long latitude();
+
+  public static Place create(String name, String website, String description, int phone, 
+        Long longitude, Long latitude) {
+    return new AutoValue_Place(name, website, description, phone, longitude, latitude);
+  }
+}
+
